f6ab1bda4e40d200d05b79b89e33702019b80ab4
g106061427 <g106061427@mail.nchu.edu.tw>
2020-01-16 21:11:14 +0800

    first stage

--- practiceCode/practiceCode.cpp
+++ practiceCode/practiceCode.cpp
@@ -3,117 +3,175 @@
 #include <string>
 #include <ctime>
 #include <algorithm>
-#define CARDSNUM 52
-
-#if 0
+#define CARDNUM 52
 using namespace std;
-class dealer {//set dealer
+
+enum SortType{
+	SORT_NONE,
+	SORT_BY_ORDER_NUM,
+	SORT_BY_ORDER_VALUE,
+	SORT_BY_SUIT
+};
+
+class Dealer{
+private:
+	int playerNum;
+	
+	vector<int> cardNum;
 public:
-	int playerNumber;//set number of players
-	void plaNum(int k) {
-		playerNumber = k;
-	}
-	vector<string> numNsuit1() {//set card number and suits
-		vector<string> Temp;
-		vector<string> cardSuit1{ "C", "D", "H", "S" };
-		for (int i = 0; i < cardSuit1.size(); i++) {
+	Dealer() {};
+	static vector<string> setNumNsuit(){
+		vector<string> temp;
+		vector<string> cardSuit{ "C", "D", "H", "S" };
+		vector<string> cardNum{ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+		for (int i = 0; i < cardSuit.size(); i++) {
 			for (int j = 2; j < 13; j++) {
-				string temp = cardSuit1[i] + " " + to_string(j + 1);
-				Temp.push_back(temp);
+				string k = cardNum[j] + " " + cardSuit[i];
+				temp.push_back(k);
 			}
 		}
 		for (int i = 1; i < 5; i++) {//set Ace and 2 become the second and first largest card in a same suit deck
-			for (int j = 1; j < 3; j++) {
-				string temp = cardSuit1[4 - i] + " " + to_string(j);
-				Temp.insert(Temp.end() + 13 * (1 - i), temp);
+			for (int j = 0; j < 2; j++) {
+				string k = cardNum[j] + " " + cardSuit[4 - i];
+				temp.insert(temp.end() + 13 * (1 - i), k);
 			}
 		}
-		return Temp;
-	}
-	vector<vector<int>> distribute1() {//Distribute the cards
-		vector<int> cardNum;
-		vector<vector<int>> Temp;
-		for (int i = 0; i < cards; i++) {
+		return temp;
+	};
+	void setPlayerNumber(int playerNum) {
+		if (playerNum <= 4 && playerNum >= 2) {
+			this->playerNum = playerNum;
+		}
+		else
+		{
+			if (playerNum > 4) {
+				cout << "Too much players." << endl;
+			}
+			if (playerNum < 2) {
+				cout << "Player needs more than one." << endl;
+			}
+			exit(0);
+		}
+	};
+	void preparingCards(){//set card number and suits
+		for (int i = 0; i < CARDNUM; i++) {
 			cardNum.push_back(i);
 		}
 		srand((unsigned)time(NULL));
 		for (int i = 0;i <= 1000;i++) {
-			int m = rand() % cards;
-			int n = rand() % cards;
+			int m = rand() % CARDNUM;
+			int n = rand() % CARDNUM;
 			swap(cardNum[m], cardNum[n]);
 		}
-		int playerCard = cards / playerNumber;//Deal the cards
-		for (int i = 0; i < playerNumber; i++) {
-			vector<int> temp;
-			for (int j = 0; j < playerCard; j++) {
-				temp.push_back(cardNum[i* playerCard+j]);
-			}
-				Temp.push_back(temp);
+	};
+	vector<int>* getCards(int player) {
+		vector<int>* temp = new vector<int>;
+		for (int i = 0; i < CARDNUM/ playerNum; i++) {
+			temp->push_back(cardNum[CARDNUM / playerNum * player + i]);
 		}
-		return Temp;
-	}
+		vector<int>::iterator it = find(temp->begin(), temp->end(), 0);
+		if (it != temp->end() && playerNum == 3) {
+			temp->push_back(cardNum[51]);
+		}
+		return temp;
+	};
 };
-class player {//set player
+
+class CardSorter {
 public:
-	vector<int> getCard(vector<int> Cards, int method) {//set the sort method
-		if (method == 0) {//by value and suit
-			sort(Cards.begin(), Cards.end());
-			return Cards;
-		}
-		else if (method == 1)//by number
-		{
-			for (int i = Cards.size() - 1; i > 0; i--)
-			{
-				for (int j = 0; j <= i - 1; j++)
-				{
-					int Q1 = Cards[j] / 13;
-					int R1 = Cards[j] % 13;
-					int Q2 = Cards[j+1] / 13;
-					int R2 = Cards[j+1] % 13;
-					if (R1 > R2)
-					{
-						swap(Cards[j], Cards[j+1]);
+	static vector<int>* sortCard(SortType type, vector<int>* cards) {
+		switch (type) {
+		case SORT_NONE:
+			return cards;
+			break;
+		case SORT_BY_ORDER_NUM:
+			for (int i = cards->size() - 1; i > 0; i--){
+				for (int j = 0; j <= i - 1; j++){
+					int Q1 = (*cards)[j] / 13;
+					int R1 = (*cards)[j] % 13;
+					int Q2 = (*cards)[j + 1] / 13;
+					int R2 = (*cards)[j + 1] % 13;
+
+					if (R1 > R2){
+						swap((*cards)[j], (*cards)[j + 1]);
 					}
-					if (R1 == R2 && Q1 > Q2) {
-						swap(Cards[j], Cards[j + 1]);
+					if (R1 == R2 && Q1 > Q2){
+						swap((*cards)[j], (*cards)[j + 1]);
 					}
 				}
 			}
-			return Cards;
+			return cards;
+			break;
+		case SORT_BY_ORDER_VALUE:
+			sort((*cards).begin(), (*cards).end());
+			return cards;
+			break;
+
+		case SORT_BY_SUIT:
+			for (int i = cards->size(); i > 0; i--) {
+				for (int j = 0; j <= i - 1; j++) {
+					int Q1 = (*cards)[j] / 13;
+					int R1 = (*cards)[j] % 13;
+					int Q2 = (*cards)[j + 1] / 13;
+					int R2 = (*cards)[j + 1] % 13;
+
+					if (R1 > R2)
+						swap(cards[j], cards[j + 1]);
+				}
+			}
+			return cards;
+			break;
+		default:
+			return cards;
+			break;
 		}
-		else
-		{
-			return Cards;
+	}
+};
+
+class Player
+{
+private:
+	string name;
+	SortType preferredSortType;
+	vector<int>* cards;
+
+public:
+	Player(SortType type, string name) {
+		preferredSortType = type;
+		this->name = name;
+	};
+	void printCards() {
+		cout << name << ": ";
+		for (int i = 0; i < cards->size();i++) {
+			cout << Dealer::setNumNsuit()[(*cards)[i]] <<"\t";
 		}
+		cout << endl;
+	};
+	void setCards(vector<int>* cards) {
+		this->cards = cards;
+		CardSorter::sortCard(preferredSortType, cards);
 	}
 };
 
 int main() {
-	int playercount = 4;//set player number
-	dealer Dealer1;//set dealer object
-	Dealer1.plaNum(playercount);
-
-	vector < vector<int> > getCards;//get cards
-	for (int i = 0;i < Dealer1.distribute1().size();i++) {
-		getCards.push_back(Dealer1.distribute1()[i]);
+	int playerNum = 4;
+	cout << "Please enter player number: " << endl;
+	cin >> playerNum;
+	Dealer dealer;
+	dealer.setPlayerNumber(playerNum);
+	dealer.preparingCards();
+	vector<Player*> playerList;
+	for (int i = 0; i < playerNum; i++)
+	{
+		string temp = "Player " + to_string(i + 1);
+		Player* player = new Player(SORT_BY_ORDER_VALUE, temp);
+		playerList.push_back(player);
 	}
-	player Player;//set player object
-	int p = cards / playercount;
-	for (int j = 0; j < playercount; j++) {//print the result
-		string name = "Player " + to_string(j+1) + "：";
-		cout << name;
-		for (int k = 0; k < p; k++) {
-				cout << Dealer1.numNsuit1()[Player.getCard(getCards[j], 0)[k]] << "\t";	
-		}
-		cout << endl;
+	for (int i = 0; i < playerNum;i++)
+	{
+		playerList[i]->setCards(dealer.getCards(i));
+		playerList[i]->printCards();
 	}
 	return 0;
 }
-#endif
-enum class sortMethods
-{
-	SORT_BY_NUMBER,
-	SORT_BY_SUIT,
-	SORT_BY_VALUE,
-	NONE,
-};
+
